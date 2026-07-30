9de1673d7535ea883781b8811b4c265a12de187d
Ben <bonczykkamil573@icloud.com>
2026-07-30 22:13:49 +0100

    Update citiesData.js

--- src/citiesData.js
+++ src/citiesData.js
@@ -723,6 +723,7 @@ export const CITIES = [
         events: [
             { date: '2015-06-23', venue: 'The Gorge Amphitheatre (Paradiso Festival)', type: 'Other', isResidentAdvisor: true },
             { date: '2017-09-16', venue: 'The Gorge Amphitheatre (ABGT250)', type: 'Milestone' },
+            { date: '2026-09-12', venue: 'The Gorge Amphitheatre (ABGT700)', type: 'Milestone' },
         ] 
     },
     { 
