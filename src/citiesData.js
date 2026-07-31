c5e0be8657d76952ffe951af7d7db65ff7e78183
Ben <bonczykkamil573@icloud.com>
2026-07-31 16:40:40 +0100

    Update citiesData.js

--- src/citiesData.js
+++ src/citiesData.js
@@ -200,6 +200,8 @@ export const CITIES = [
             { date: '2012-08-04', venue: 'Dance Valley Festival', type: 'Other', isSongkick: true },
             { date: '2015-01-30', venue: 'Paradiso', type: 'Tour' },
             { date: '2016-09-24', venue: 'Ziggo Dome (ABGT200)', type: 'Milestone' },
+            { date: '2022-05-12', venue: 'Gashouder', type: 'Other' },
+            { date: '2022-05-13', venue: 'Gashouder', type: 'Other' },
             { date: '2025-10-31', venue: 'AFAS Live', type: 'Tour' },
         ]
     },
